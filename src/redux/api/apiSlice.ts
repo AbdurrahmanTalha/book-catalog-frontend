77cc108624fa0304e448fae97bb61fa0ea495f8b
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-07-16 21:43:42 +0600

    added vercel.json

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -75,7 +75,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/v1",
+        baseUrl: import.meta.env.VITE_BACKEND_URL as string,
         prepareHeaders: (headers) => {
             headers.set("Content-Type", "application/json")
 
